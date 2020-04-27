d8f8a73f57d1986ab8cb12a897e2ff9f8c7564ac
TerryLun <tianweilun@yahoo.com>
2020-04-26 22:29:30 -0700

    0426

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+# database setup
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+"""
+create the db file by going to terminal:
+>>> from app import db
+>>> db.create_all()
+
+in terminal:
+    add entries:
+    >>> db.session.add(ModelName(**kwargs))
+    
+    return all entries:
+    >>> ModelName.query.all()
+"""
+
+
+# database schema/model
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)  # up to 100 characters
+    content = db.Column(db.Text, nullable=False)  # Text has no length limit
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post ' + str(self.id)
+
 
 all_posts = [
     {
